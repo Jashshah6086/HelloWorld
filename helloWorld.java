14d9e4e130b9ad061c25ad7848548fb75b62e9d1
mohammed afif ahmed <51887071+afif1400@users.noreply.github.com>
2021-05-27 10:07:47 +0530

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld{
+  public static void main(string[] args){
+    System.out.println("hello from the world of java");
+  }
+}
+
+// To run this file install java and use 
+// javac helloworld.java : then
+// java HelloWorld
